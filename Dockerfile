0119196fbbe40f5bdb10d01ce57753f89407bc7d
Maik Kluwe <m.kluwe92@googlemail.com>
2022-02-06 15:33:56 +0100

    Inital commit

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM ubuntu:20.04
+
+RUN apt-get update && apt-get upgrade -y
+RUN apt-get install -y build-essential git curl zip inotify-tools python
+
+RUN curl -sL https://deb.nodesource.com/setup_10.x | bash - && apt-get install -y nodejs
+
+RUN git clone https://github.com/detroxx92/ttfkit.git ttfkit \
+    && cd ttfkit \
+    && npm install
+
+WORKDIR /ttfkit
+
+CMD [ "node", "/ttfkit/server.js" ]
---
+++ index.js
@@ -0,0 +1,72 @@
+const _ = require('lodash');
+const fs = require('fs');
+const path = require('path');
+const punycode = require('punycode/');
+
+const SVGIcons2SVGFontStream = require('svgicons2svgfont');
+const svg2ttf = require('svg2ttf');
+
+function ttfkit(fontConfig, output_function) {
+    
+    let fontName = fontConfig.fontName;
+
+    let tmp_folder = "temp";
+    if (!fs.existsSync(tmp_folder))
+        fs.mkdirSync(tmp_folder);
+
+    // starting unicode
+    let unicode = 0xEA01;
+
+    let font_stream = new SVGIcons2SVGFontStream({fontName: fontName});
+    let output_svg = path.join(tmp_folder, 'output.svg');
+    let items = [];
+
+    // iterate files in input_folder
+    _.forEach(fontConfig.files, file => {
+        let filePath = path.join(tmp_folder, file.name);
+        fs.writeFileSync(filePath, Buffer.from(file.data, 'base64'));
+
+        // create glyph from svg
+        const glyph = fs.createReadStream(filePath);
+        glyph.metadata = {
+            unicode: [ punycode.ucs2.encode([unicode]) ],
+            name: file.name,
+        };
+        font_stream.write(glyph);
+
+        items.push({
+            path: filePath,
+            name: file.name,
+            code: unicode.toString(16).toUpperCase()
+        });
+
+        // increment unicode character
+        unicode +=1;
+    });
+
+    font_stream
+    .pipe(fs.createWriteStream(output_svg))
+    .on('finish', () => {
+        // convert svg font to ttf file format
+        let ttfData = svg2ttf(fs.readFileSync(output_svg, 'utf-8'), {});
+        output_function(new Buffer.from(ttfData.buffer));
+        //TODO: fix
+        //res.json({
+        //    name: fontName,
+        //    data: new Buffer.from(ttfData.buffer).toString('base64')
+        //});
+
+        // cleanup
+        fs.rmSync(output_svg);
+        _.forEach(items, (item) => {
+            fs.rmSync(item.path);
+        });
+    })
+    .on('error', (err) => {
+        console.log(err);
+    });
+
+    font_stream.end();
+}
+
+module.exports = ttfkit;
